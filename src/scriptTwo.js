c582838cef125e4f207e6380d249e17f027cae05
RajdipChatterjee <rajdipchatterjee3000@gmail.com>
2025-09-16 02:18:36 +0530

    another script added with new edit button ui for all tasks and new delete button ui

---
+++ src/scriptTwo.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let todoInput = document.getElementById("todo-input");
+  let addTask = document.getElementById("add-task-btn");
+  let todoList = document.getElementById("todo-list");
+
+  let tasks = JSON.parse(localStorage.getItem('tasks')) ?? [];
+  tasks.forEach((task)=>renderTask(task));
+
+  addTask.addEventListener("click", () => {
+    let task = todoInput.value.trim();
+    if (task == "") return;
+    const taskObj = {
+      id: Date.now(),
+      text: task,
+      completed: false,
+    };
+    tasks.push(taskObj);
+    saveTask(taskObj);
+  });
+
+  function saveTask(taskObj) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTask(taskObj);
+  }
+
+  function renderTask(task) {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+    <span class="grow">${task.text}</span>
+    <div>
+        <button id="deleteButton" class="bg-blue-600 p-1 rounded-sm duration-300 hover:bg-blue-500">
+            <img src="./assets/edit-svgrepo-com.svg" />
+        </button>
+        <button id="deleteButton" class="bg-red-600 p-1 rounded-sm duration-300 hover:bg-red-500">
+            <img src="./assets/trash-bin-trash-svgrepo-com.svg" />
+        </button>
+    </div>
+    `;
+    todoList.append(li);
+  }
+
+  tasks.forEach((value) => {
+    
+  })
+});
